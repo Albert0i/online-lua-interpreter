ce349436821160f561a07c7bb5134740d488611b
Alberto Iong <albert0i@hotmail.com>
2025-08-13 16:20:59 +0800

    add 3 more lua scripts

---
+++ src/lua/fib.lua
@@ -0,0 +1,18 @@
+--[[
+	fib demo
+]]
+local number = tonumber(ARGV[1]) or 10
+
+local function fib(n)
+  local t = {}
+  local a, b = 0, 1
+
+  for i = 1, n do
+    t[i] = a
+    a, b = b, a + b
+  end
+
+  return t
+end
+
+return fib(number)
