cea9c9bb9037cf626dedfc41645de68409a2b8a4
Alberto Iong <albert0i@hotmail.com>
2025-08-13 15:22:25 +0800

    title click fix

---
+++ src/lua/readme.lua
@@ -0,0 +1,14 @@
+--[[
+	online-lua-interpreter
+
+	Git repository 
+	https://github.com/Albert0i/online-lua-interpreter/tree/main
+
+	Online demo
+	https://lua-interpreter.onrender.com/
+
+	"never let yourself be so far deceived as to doubt that."
+	The Castle by Franz Kafka
+]]
+
+return redis.status_reply('Ok')
---
+++ src/lua/seed.lua
@@ -0,0 +1,13 @@
+--[[
+	Small script to seed database
+	Parameters: None
+]]
+redis.call("HSET", "OLI:scripts:init.lua", "code", "return 'init'", "updatedAt", "2025-08-12 11:02:01.769", "updateIdent", "0" )
+
+redis.call("HSET", "OLI:scripts:do_this.lua", "code", "return 'do this'", "updatedAt", "2025-08-12 11:02:01.769", "updateIdent", "0" )
+
+redis.call("HSET", "OLI:scripts:do_that.lua", "code", "return 'do that'", "updatedAt", "2025-08-12 11:02:01.769", "updateIdent", "0" )
+
+redis.call("HSET", "OLI:scripts:do_nothing.lua", "code", "return 'do nothing'", "updatedAt", "2025-08-12 11:02:01.769", "updateIdent", "0" )
+
+return redis.status_reply('Ok')
